83498677ebb85b2b58e8f3305a78e90627eff503
Sofus <Sofus@SOFUS-PC>
2025-03-19 18:56:19 +0100

    tester program

--- CSharpBegynderOpgave_4/Program.cs
+++ CSharpBegynderOpgave_4/Program.cs
@@ -1,11 +1,31 @@
 ﻿//Opgradér spillet fra Opgave_3 med et Loop sådan at man får flere forsøg når man gætter forkert
 
-int input = covert.ToInt32
+int input = convert.ToInt32(Console.readline)
 	
-Console.writeline(input + 
+ 
 
 
 	console.writeline("inputtet er = 2");
 
 Console.ReadKey();
 
+Random random = new Random();
+int Resultat = random.Next(0, 101); 
+Console.WriteLine(Resultat);
+
+
+int a = 1;
+int b = 2;
+
+if (a > b)
+{
+    Console.WriteLine("A er større end B");
+}
+else if (a < b)
+{
+    Console.WriteLine("A er mindre end B");
+}
+else
+{
+    Console.WriteLine("A og B er ens!");
+}
